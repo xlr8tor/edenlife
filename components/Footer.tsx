fc2f593b4b5268d84d7cbdf882aef24f6ef91f31
Jeremiah Charles <charlesjeremiah89@yahoo.com>
2026-02-05 12:45:06 +0000

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -217,7 +217,7 @@ export default function Footer() {
               </SocialButton>
             </Stack>
             <Text fontSize={"sm"}>
-              © 2021 Eden Life Inc. All rights reserved.
+              © 2025 Eden Life Inc. All rights reserved.
             </Text>
           </Stack>
         </SimpleGrid>
